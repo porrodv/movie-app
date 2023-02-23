b7ebb99533d2b4f1f7bf09f83f800a09ed8501f3
Alexander Porro <alex1020pl@gmail.com>
2023-02-23 13:55:32 -0500

    404 y navbar opciones

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 import { starIcon } from "./data/icons.js";
 
-const API_KEY = "04c35731a5ee918f014970082a0088b1"; // https://github.com/florinpop17/10-projects-10-hours/blob/master/movie-app/script.js
-const POPULAR_API = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`;
+const API_KEY = "04c35731a5ee918f014970082a0088b1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&page=1&query=`;
-
+const POPULAR_API = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`;
+const UPCOMING_API = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=es-ES&page=1`;
+const TOPRANKED_API = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=es-ES&page=1`;
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
 getMovies(POPULAR_API);
 
-async function getMovies(url) {
-  const response = await fetch(url);
-  const data = await response.json();
+const populars = document.getElementById("populars");
+const latests = document.getElementById("latests");
+const topranked = document.getElementById("topranked");
+const sections = [populars, latests, topranked];
 
-  console.log(data);
-  showMovies(data.results);
-}
+sections.forEach((section) => {
+  section.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (section === populars) {
+      getMovies(POPULAR_API);
+      return;
+    }
+    if (section === latests) {
+      getMovies(UPCOMING_API);
+      return;
+    }
+    if (section === topranked) {
+      getMovies(TOPRANKED_API);
+      return;
+    }
+  });
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -29,6 +45,18 @@ form.addEventListener("submit", (e) => {
     search.value = "";
   }
 });
+
+async function getMovies(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // console.log(data);
+    showMovies(data.results);
+  } catch {
+    showError404();
+  }
+}
 
 function showMovies(movies) {
   main.innerHTML = "";
@@ -51,9 +79,7 @@ function showMovies(movies) {
                     ${starIcon}
                 </span>
             </div>
-            
         `;
-
     main.appendChild(movieContainer);
   });
 }
@@ -66,4 +92,16 @@ function getClassByRate(vote) {
   } else {
     return "red";
   }
+}
+
+function showError404(){
+  main.innerHTML = "";
+    const errorContainer = document.createElement("div");
+    errorContainer.classList.add("error404");
+    errorContainer.innerHTML = `
+            <h1>ERROR 404</h1>
+            <h2>Puede deberse a un problema con el servidor</h2>
+            <img src="./img/error404.png">
+        `;
+    main.appendChild(errorContainer);
 }
